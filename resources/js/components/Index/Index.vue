1857b4eede138d0c4467c21c7d3f5644cbd442f4
seiyu-nico <yuseiisii141+git@gmail.com>
2020-03-25 08:46:11 +0900

    topからのリンク作成

--- resources/js/components/Index/Index.vue
+++ resources/js/components/Index/Index.vue
@@ -1,15 +1,14 @@
 <template>
   <div>
     <h1>Index</h1>
-    <div  v-if="isLogin">
+    <div v-if="isLogin">
       <div>
         <button type="button" class="btn btn-outline-info" @click="logout">ログアウト</button>
       </div>
       {{ nickname }}
     </div>
-    <div v-else>
-    　<router-link to="/login">ログイン</router-link>
-    </div>
+      <router-link :to="{name: 'Index.Index'}">TOP</router-link>
+      <router-link :to="{name: 'Memo.Index'}">メモ</router-link>
   </div>
 </template>
 <script>
