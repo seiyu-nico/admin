ca7f10feebd953a6fcf36d75e9862e7f58536e91
seiyu-nico <yuseiisii141+git@gmail.com>
2020-06-18 21:21:46 +0900

    metaタグ修正

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,10 +2,10 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
   <title>{{ config('app.name') }}</title>
-
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <!-- Scripts -->
   <script src="{{ mix('js/app.js') }}" defer></script>
-
 
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
